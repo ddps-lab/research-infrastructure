d88097b4078ce7670ed784aca4df93053277020a
최재강 <workd.official@gmail.com>
2023-02-24 20:07:35 +0900

    add docker

--- setup.sh
+++ setup.sh
@@ -1,4 +1,3 @@
-sudo apt-get update
 sudo apt-get update && sudo apt-get install -y apt-transport-https curl
 
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
@@ -7,8 +6,7 @@ cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
 deb https://apt.kubernetes.io/ kubernetes-xenial main
 EOF
 
-sudo apt-get update
-sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-get install -y kubelet kubeadm kubectl docker
 
 # 패키지 버전 홀드 (업데이트에서 제외)
-sudo apt-mark hold kubelet kubeadm kubectl 
+sudo apt-mark hold kubelet kubeadm kubectl docker
