2d758dce4f9aa8ea93c5f14e83535ce14ded0fda
최재강 <workd.official@gmail.com>
2023-02-26 21:45:29 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,22 +6,8 @@ cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
 deb https://apt.kubernetes.io/ kubernetes-xenial main
 EOF
 
-sudo apt-get install \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release
-
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-
-echo \
- "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
- $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
-
 sudo apt-get install -y kubelet kubeadm kubectl
 
 # 패키지 버전 홀드 (업데이트에서 제외)
-sudo apt-mark hold kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl 
