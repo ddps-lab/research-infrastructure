a76bae76c686c8955d6eb1ce7f21c4189273d36d
최재강 <workd.official@gmail.com>
2023-02-26 23:51:46 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,6 +2,8 @@ sudo apt-get update && sudo apt-get install -y apt-transport-https curl
 
 sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
 echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo tee /etc/apt/sources.list.d/kubernetes.list
+
 sudo apt-get update
 sudo apt-get install -y kubelet kubeadm kubectl
 
