d1f802be2cf033cdc0cf3493507458a8d40fc8f6
최재강 <workd.official@gmail.com>
2023-03-06 15:57:40 +0900

    calico 적용

--- kubectl.sh
+++ kubectl.sh
@@ -4,3 +4,6 @@ sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+# calico cni 생성
+kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.25.0/manifests/tigera-operator.yaml
