48ef11659fe8a7a87c69e0099bc8e519e287edb7
최재강 <workd.official@gmail.com>
2023-03-07 18:09:44 +0900

    add bentoml

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 sudo apt-get update
  
-sudo apt-get install \
+sudo apt-get install -y \
     ca-certificates \
     curl \
     gnupg \
@@ -15,10 +15,12 @@ echo \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
 sudo systemctl enable docker
 sudo systemctl start docker
+
+pip install git+https://github.com/bentoml/bentoml
 
 # Swap off
 sudo swapoff -a && sudo sed -i '/swap/s/^/#/' /etc/fstab
