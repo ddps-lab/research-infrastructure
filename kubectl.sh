7cc4447473f2a64c4ddb28670aa66cb7e1e18563
최재강 <workd.official@gmail.com>
2023-03-07 19:45:07 +0900

    kube-flannel 최신화

--- kubectl.sh
+++ kubectl.sh
@@ -6,4 +6,4 @@ sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
 # calico cni 생성
-kubectl apply -f https://raw.githubusercontent.com/flannel-io/flannel/master/Documentation/kube-flannel.yml
+kubectl apply -f https://github.com/flannel-io/flannel/releases/latest/download/kube-flannel.yml
