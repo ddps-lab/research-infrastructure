57be216ae13effb4763a93cf87f06b92d687abde
manchann <chl8273@naver.com>
2023-03-24 15:04:43 +0900

    worker node ebs 볼륨 추가

--- infra/k8s/worker_node/worker.tf
+++ infra/k8s/worker_node/worker.tf
@@ -4,10 +4,15 @@ resource "aws_instance" "worker_node" {
   instance_type          = var.instance_type
   iam_instance_profile   = var.ec2_instance_profile
   key_name               = var.key_name
-  subnet_id              = var.private_subnet_ids[count.index%length(var.private_subnet_ids)]
+  subnet_id              = var.private_subnet_ids[count.index % length(var.private_subnet_ids)]
   source_dest_check      = false
   vpc_security_group_ids = [var.cluster_sg_id]
   tags = {
     "Name" = "${var.cluster_prefix}-worker-${count.index}"
+  }
+  root_block_device {
+    volume_size           = 50    # 볼륨 크기를 지정합니다.
+    volume_type           = "gp2" # 볼륨 유형을 지정합니다.
+    delete_on_termination = true  # 인스턴스가 종료될 때 볼륨도 함께 삭제되도록 설정합니다.
   }
 }
