61415204d6047d29912bdd07afba6b0be6a7db1b
manchann <chl8273@naver.com>
2023-03-26 17:19:00 +0900

    bentoml code 변경

--- mlserving/bentoml_predict.py
+++ mlserving/bentoml_predict.py
@@ -3,6 +3,7 @@ import bentoml
 from bentoml.io import NumpyNdarray, Image
 from PIL.Image import Image as PILImage
 from torchvision.models import resnet50, ResNet50_Weights
+import time
 
 weights = ResNet50_Weights.DEFAULT
 model = resnet50(weights=weights)
@@ -11,8 +12,8 @@ model.eval()
 
 
 saved_model = bentoml.pytorch.save_model(
-    "bentoml_resnet50", # model name in the local model store
-    model, # model instance being saved
+    "bentoml_resnet50",  # model name in the local model store
+    model,  # model instance being saved
 )
 
 print(f"Model saved: {saved_model}")
@@ -21,9 +22,13 @@ runner = bentoml.pytorch.get("bentoml_resnet50").to_runner()
 
 svc = bentoml.Service("pytorch_resnet50", runners=[runner])
 
+
 @svc.api(input=Image(), output=NumpyNdarray(dtype="int64"))
-def predict(input_img: PILImage):
+async def predict(input_img: PILImage):
+    start_time = time.time()
     img_arr = np.array(input_img)/255.0
     input_arr = np.expand_dims(img_arr, 0).astype("float32")
-    output_tensor = runner.predict.run(input_arr)
-    return output_tensor.numpy()
+    input_arr = np.transpose(input_arr, (0, 3, 1, 2))
+    output_tensor = await runner.async_run(input_arr)
+    inference_time = time.time() - start_time
+    return inference_time
