717820864a21a60a6ef43aaa26076df1fcc96d0d
mhsong <mhsong@kookmin.ac.kr>
2023-04-06 11:05:08 +0900

    - Add EFS module

---
+++ infra/efs/variables.tf
@@ -0,0 +1,4 @@
+variable "vpc" {}
+variable "private_subnet_ids" {}
+variable "cluster_prefix" {}
+variable "efs_for" {}
--- infra/main.tf
+++ infra/main.tf
@@ -15,22 +15,34 @@ module "vpc" {
   ubuntu_ami           = data.aws_ami.ubuntu_ami
   key_name             = var.key_name
   ec2_instance_profile = aws_iam_instance_profile.nat-ec2role-instance-profile.name
-  cluster_prefix        = "${var.main_suffix}-k8s"
+  cluster_prefix       = "${var.main_suffix}-k8s"
+}
+
+module "efs-monitoring" {
+  source             = "./efs"
+  efs_for            = "monitoring"
+  cluster_prefix     = "${var.main_suffix}-k8s"
+  vpc                = module.vpc.vpc
+  private_subnet_ids = module.vpc.private_subnet_ids
+  depends_on = [
+    module.vpc
+  ]
 }
 
 module "k8s" {
-  source                = "./k8s"
-  cluster_prefix        = "${var.main_suffix}-k8s"
-  vpc                   = module.vpc.vpc
-  public_subnet_ids     = module.vpc.public_subnet_ids
-  private_subnet_ids    = module.vpc.private_subnet_ids
-  master_node_number    = var.master_node_number
-  worker_node_number    = var.worker_node_number
-  instance_type         = var.instance_type
-  ubuntu_ami            = data.aws_ami.ubuntu_ami
-  key_name              = var.key_name
-  ec2_instance_profile  = aws_iam_instance_profile.k8s-cluster-ec2role-instance-profile.name
+  source               = "./k8s"
+  cluster_prefix       = "${var.main_suffix}-k8s"
+  vpc                  = module.vpc.vpc
+  private_subnet_ids   = module.vpc.private_subnet_ids
+  master_node_number   = var.master_node_number
+  worker_node_number   = var.worker_node_number
+  instance_type        = var.instance_type
+  ubuntu_ami           = data.aws_ami.ubuntu_ami
+  key_name             = var.key_name
+  ec2_instance_profile = aws_iam_instance_profile.k8s-cluster-ec2role-instance-profile.name
+  monitoring-efs-id    = module.efs-monitoring.efs-id
   depends_on = [
-    module.vpc
+    module.vpc,
+    module.efs-monitoring
   ]
 }
