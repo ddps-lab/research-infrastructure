c93b0acae22e1bb33514c58f6a81b476ac2713a9
manchann <chl8273@naver.com>
2023-05-23 16:51:26 +0900

    add alb

--- eks/alb.tf
+++ eks/alb.tf
@@ -1,4 +1,26 @@
+resource "null_resource" "iam-oidc-provider" {
+  provisioner "local-exec" {
+    command = "eksctl utils associate-iam-oidc-provider --region ${var.region} --cluster ${var.cluster_name} --approve"
+  }
+}
+resource "helm_release" "cert_manager" {
+  chart      = "cert-manager"
+  repository = "https://charts.jetstack.io"
+  name       = "cert-manager"
+
+  create_namespace = "cert_manager"
+  namespace        = "cert_manager"
+
+  set {
+    name  = "installCRDs"
+    value = "true"
+  }
+}
+
 resource "helm_release" "ingress" {
+  depends_on = [
+    helm_release.cert_manager
+  ]
   name       = "ingress"
   chart      = "aws-load-balancer-controller"
   repository = "https://aws.github.io/eks-charts"
